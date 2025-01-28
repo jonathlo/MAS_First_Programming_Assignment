1622dd37d2459fe4b8da8cc6a1c192f604478bb5
Arya Bhargava <92285621+aryabhargava15@users.noreply.github.com>
2025-01-28 15:31:56 -0500

    Update index.jsx

--- Mobile-App/app/index.jsx
+++ Mobile-App/app/index.jsx
@@ -116,4 +116,59 @@ export default function Index() {
   );
 }
 
-
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "black",
+  },
+  inputContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginBottom: 10,
+    padding: 10,
+    width: "100%",
+    maxWidth: 1024,
+    marginHorizontal: "auto",
+  },
+  input: {
+    flex: 1,
+    borderColor: "gray",
+    borderWidth: 1,
+    borderRadius: 5,
+    padding: 10,
+    marginRight: 10,
+    fontSize: 18,
+    minWidth: 0,
+    color: "white",
+  },
+  addButton: {
+    backgroundColor: "white",
+    borderRadius: 5,
+    padding: 10,
+  },
+  addButtonText: {
+    fontSize: 18,
+    color: "black",
+  },
+  todoItem: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    gap: 4,
+    padding: 10,
+    borderBottomColor: "gray",
+    borderBottomWidth: 1,
+    width: "100%",
+    maxWidth: 1024,
+    marginHorizontal: "auto",
+  },
+  todoText: {
+    flex: 1,
+    fontSize: 18,
+    color: "white",
+  },
+  completedText: {
+    textDecorationLine: "line-through",
+    color: "gray",
+  },
+});
